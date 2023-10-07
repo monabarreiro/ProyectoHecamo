f5763f05c4826cf666d37163affdc0ccbf6f3916
monabarreiro <barreiro.monica@gmail.com>
2023-10-07 15:08:36 -0300

    Feat: interact button

--- app/src/main/java/com/example/proyectohecamo/MainActivity.java
+++ app/src/main/java/com/example/proyectohecamo/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.proyectohecamo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button myButton = findViewById(+R.id.button);
     }
+        public void onClick(View view) {
+
+            TextView helloWorldtext = findViewById(+R.id.helloWorldtext);
+            helloWorldtext.setText("Boton clickeado");
+            helloWorldtext.setBackgroundColor(+R.color.green);
+        }
+
+
+
+
+
 }

e3fa971f66a1da7386e153f633dfbb1afac1aa00
monabarreiro <barreiro.monica@gmail.com>
2023-10-11 16:48:27 -0300

    Feat: change name button Busqueda Enfermedades from Cultivos

--- app/src/main/java/com/example/proyectohecamo/MainActivity.java
+++ app/src/main/java/com/example/proyectohecamo/MainActivity.java
@@ -1,31 +1,46 @@
 package com.example.proyectohecamo;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.TextureView;
 import android.view.View;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button myButton = findViewById(+R.id.button);
+        Button myButton = findViewById(+R.id.button_busq_cultivos);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView maiz = findViewById(+R.id.maiz);
+                maiz.setText(R.string.maiz);
+
+                TextView soja = findViewById(+R.id.soja);
+                soja.setText(R.string.soja);
+
+                TextView limon = findViewById(+R.id.limon);
+                limon.setText(R.string.limon);
+
+            }
+
+
+
+
+
+
+            });
+
+
+
     }
-        public void onClick(View view) {
-
-            TextView helloWorldtext = findViewById(+R.id.helloWorldtext);
-            helloWorldtext.setText(R.string.Cultivos);
-            helloWorldtext.setBackgroundColor(+R.color.green);
-        }
-
-
-
 
 
 }

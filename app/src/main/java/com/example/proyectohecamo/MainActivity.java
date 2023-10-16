f572d7f298b04905ef0049c1169a3f3a663053fc
monabarreiro <barreiro.monica@gmail.com>
2023-10-15 21:27:30 -0300

    Feat: 2da.ActivityMaiz

--- app/src/main/java/com/example/proyectohecamo/MainActivity.java
+++ app/src/main/java/com/example/proyectohecamo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.proyectohecamo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.view.TextureView;
 import android.view.View;
 import android.os.Bundle;
@@ -11,23 +12,35 @@ import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+ Button buttonMaiz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button myButton = findViewById(+R.id.buttonMaiz);
-        myButton.setOnClickListener(new View.OnClickListener() {
-
+        buttonMaiz = findViewById(R.id.buttonMaiz);
+        buttonMaiz.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick (View v) {
+               Intent intent = new Intent(MainActivity.this,Activity2Maiz.class); // aca  van a ir el activity desde la que estoy sliendo ala
+                // que quiero ir y creo ademas el activity nuevo
+                // inicio la segunda activity
+                startActivity(intent);
+            }
+        });
+
+      //  Button myButton = findViewById(+R.id.buttonMaiz);
+      //  myButton.setOnClickListener(new View.OnClickListener() {
+
+          //  @Override
+           // public void onClick(View view) {
 
 
-                TextView maiz = findViewById(+R.id.buttonMaiz);
-                maiz.setText(R.string.maiz);
-                maiz.setBackgroundColor(+R.color.naranja);
-                Toast.makeText(getApplicationContext(),getString(R.string.naranja),Toast.LENGTH_LONG);
+          //      TextView maiz = findViewById(+R.id.buttonMaiz);
+          //      maiz.setText(R.string.maiz);
+          //      maiz.setBackgroundColor(+R.color.naranja);
+            //    Toast.makeText(getApplicationContext(),getString(R.string.naranja),Toast.LENGTH_LONG);
 
 
 
@@ -37,14 +50,10 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
-
-            });
+    //   });
 
 
 
     }
 
 
-}
